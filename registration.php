ca449a011b6ff12b366df22caa54779e0475e7f4
Jerke Combee <jerke1988@gmail.com>
2025-05-16 14:00:32 +0200

    move files to src dir

--- registration.php
+++ registration.php
@@ -3,5 +3,5 @@
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
     'Elgentos_SmileDebugBarElasticsuite',
-    __DIR__
+    __DIR__ . '/src'
 );
